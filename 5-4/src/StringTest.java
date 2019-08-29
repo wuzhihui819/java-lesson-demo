a35aa0c78edf059de56069b4872046486794ae19
wuzhihui819 <wuzhihui819@163.com>
2019-08-29 21:45:20 +0800

    [ADD] add 5-4

---
+++ 5-4/src/StringTest.java
@@ -0,0 +1,16 @@
+public class StringTest {
+	public static void main(String[] args) {
+		String s = "This is a test String!";
+		System.out.println("before changed, s = " + s);
+		String t = s.toLowerCase();
+		System.out.println("t=" + t);
+		StringBuffer strb = new StringBuffer(s);
+		System.out.println("s.length = " + s.length());
+		System.out.println("strb.length = " + strb.length());
+		t = s.replace('a', 'o');
+		System.out.println("s3.replace=" + t);
+		StringBuffer s3s3t = strb.replace(2, 4, "at");
+		System.out.println("strb.replace=" + s3s3t);
+		System.out.println("strb.capacity = " + strb.capacity());
+	}
+}
